064db658d44f66b6a975750a9ce39ba25a1b3bbd
rad <samuelcostabr5@gmail.com>
2026-05-28 13:14:35 -0300

    arrumando Merge branch 'main'2

--- Server/app.js
+++ Server/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const db = require('./database/db');
@@ -18,27 +17,4 @@ app.get('/', (req, res) => {
 
 app.listen(3000,() => {
     console.log('Servidor rodando na porta 3000');
-=======
-const express = require('express');
-const app = express();
-const db = require('./database/db');
-
-const alunosRoutes = require('./routes/alunosRoutes');
-
-app.use(express.json());
-app.use(express.static('public'));
-
-
-app.use('/alunos',alunosRoutes);
-
-// Rota para obter todos os alunos
-
-app.get('/', (req, res) => {
-    res.send('Servidor funcionando');
-});
-
-
-app.listen(3000,() => {
-    console.log('Servidor rodando na porta 3000');
->>>>>>> main
 });
--- Server/database/db.js
+++ Server/database/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
@@ -15,22 +14,4 @@ connection.connect((err)=>{
     console.log('Conexao bem sucedida ao banco de dados!');
 })
 
-=======
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'123456',
-    database:'escola'
-})
-connection.connect((err)=>{
-    if(err){{
-        console.error('Erro ao conectar ao banco de dados: ', err);
-        return;
-    }}
-    console.log('Conexao bem sucedida ao banco de dados!');
-})
-
->>>>>>> main
 module.exports = connection;
